e1ea09eece61ca6f9e309cfd5c5f254e1e6f2f2d
ajwilsonvt <ajwilsonvt@users.noreply.github.com>
2018-02-09 20:24:15 -0500

    Update users.js

--- app/routes/api/users.js
+++ app/routes/api/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json({ "status":"success" });
+  res.json({ "status": "success" });
 });
 
 module.exports = router;
